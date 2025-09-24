073c54e1e59337266703a197a3be97f257c0ad21
Franaffel <franksbass19@gmail.com>
2025-09-24 13:32:58 +0800

    ah

--- synap/Characters/Hedler/hedler.gd
+++ synap/Characters/Hedler/hedler.gd
@@ -5,13 +5,15 @@ const Lantern = preload("res://Characters/Hedler/Hedler-lantern.tscn")
 func _ready():
 	$AttackArea1.body_entered.connect(_on_attack_area_entered.bind(0))
 	$AttackArea2.body_entered.connect(_on_attack_area_entered.bind(1))
+	$AttackArea3.body_entered.connect(_on_attack_area_entered.bind(2))
 	$"../../UI".get_node("Healthbar").init_health(MaxHP)
 	attack_areas= [
 	$AttackArea1,
-	$AttackArea2
+	$AttackArea2,
+	$AttackArea3
 	]
-	attack_damage =  [8,12]
-	combo_count = 2
+	attack_damage =  [8,8, 12]
+	combo_count = 3
 
 func skill():
 	attackState = AttackState.ATTACKING
